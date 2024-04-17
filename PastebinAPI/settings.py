a52ce4533394b6b81d6f359a12f5a8c0b32b3862
Yander Sanchez <sanchezyander@gmail.com>
2024-04-17 10:39:02 -0400

    fix: web API return 500 staticfiles.E002

--- PastebinAPI/settings.py
+++ PastebinAPI/settings.py
@@ -156,9 +156,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media-files")
 MEDIA_URL = "media/"
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
