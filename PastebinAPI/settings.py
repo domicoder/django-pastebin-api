fba09a76ad4f1c79fc905520a8281557750cd783
Yander Sanchez <sanchezyander@gmail.com>
2024-04-17 09:44:34 -0400

    fix: statics files

--- PastebinAPI/settings.py
+++ PastebinAPI/settings.py
@@ -160,8 +160,17 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media-files")
 
-MEDIA_URL = 'media/'
-STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "..", "dist", "static"),
+]
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
