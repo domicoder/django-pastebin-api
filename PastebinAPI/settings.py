168fc6fd4d65ae7c5b452cbfcfb180a8f50c40a4
Yander Sanchez <sanchezyander@gmail.com>
2024-04-17 07:08:31 -0400

    feat: secure secret key in env

--- PastebinAPI/settings.py
+++ PastebinAPI/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kjtq0h89&q&cbj%^sliltpdv&vwu2cq4b+&q8qae87g42k%szw'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
